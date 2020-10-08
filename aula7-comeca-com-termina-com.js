25ed2e7651863d77c526a92381e0b7c965dce897
Renato Xavier <rnatu91@gmail.com>
2020-10-08 15:23:10 -0300

    Regexp - Aula 6 (Lookahead e Lookbehind(Lookaround))
    Regexp - Aula 6 (Lookahead e Lookbehind(Lookaround))

--- aula7-comeca-com-termina-com.js
+++ aula7-comeca-com-termina-com.js
@@ -7,9 +7,8 @@ const { cpfs2 } = require('./base');
 
 const cpf = '254.224.877-45'
 const regexp = /^(\d{3}\.){2}\d{3}\-\d{2}$/g;
+console.log(cpf.match(regexp))
 
 
 const regexp2 = /^(\d{3}\.){2}\d{3}\-\d{2}$/gm;
-
-console.log(cpf.match(regexp))
 console.log(cpfs2.match(regexp2))
